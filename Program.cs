626d9922448d398a935357e9a73affd3db7406db
Daniel Valcarce <davamix@gmail.com>
2016-07-31 14:10:42 +0200

    Basic sample about Dapper.net with Sqlite

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DapperSample
+{
+    public class Program
+    {
+	    private static SQLiteDataProvider _connection;
+
+	    public static void Main(string[] args)
+        {
+			_connection = new SQLiteDataProvider("Data Source=Sample.db");
+		    _connection.CrateTable();
+
+		    string option = string.Empty;
+		    do
+		    {
+			    Menu();
+			    option = Console.ReadLine();
+			    Execute(option);
+
+		    } while (!option.Equals("exit"));
+        }
+
+	    
+
+	    private static void Menu()
+	    {
+		    Console.WriteLine("1. Get all Foo values");
+		    Console.WriteLine("2. Add new Foo value (random)");
+		    Console.Write("\n(type exit to finish)\n=> ");
+	    }
+
+		private static void Execute(string option)
+		{
+			int optionNumber;
+			if (int.TryParse(option, out optionNumber))
+			{
+				switch (optionNumber)
+				{
+					case 1:
+						GetAll();
+						break;
+					case 2:
+						Save();
+						break;
+
+				}
+			}
+		}
+
+	    private static void GetAll()
+	    {
+		    var foos = _connection.GetAll();
+
+			if(!foos.Any())
+				Console.WriteLine("No values");
+
+		    foreach (var foo in foos)
+		    {
+				Console.WriteLine($"[{foo.Id}]:: {foo.Name}");
+			}
+
+		    Console.WriteLine();   
+	    }
+
+		private static void Save()
+		{
+			var foo = new Foo
+			          {
+				          Id = Guid.NewGuid().ToString(),
+				          Name = DateTime.UtcNow.ToString("G")
+			          };
+
+			_connection.Save(foo);
+
+			Console.WriteLine($"Value saved: {foo.Id}");
+			Console.WriteLine();
+		}
+	}
+}
---
+++ SQLiteDataProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace DapperSample
+{
+    public class SQLiteDataProvider
+    {
+	    private SqliteConnection _connection;
+
+	    public SQLiteDataProvider(string connectionString)
+	    {
+		    _connection = new SqliteConnection(connectionString);
+	    }
+
+
+	    public void CrateTable()
+	    {
+			_connection.Open();
+		    _connection.Execute("CREATE TABLE IF NOT EXISTS Foo(Id TEXT PRIMARY KEY NOT NULL, Name TEXT)");
+			_connection.Close();
+	    }
+
+	    public IEnumerable<Foo> GetAll()
+	    {
+		    _connection.Open();
+		    try
+		    {
+			    var values = _connection.Query<Foo>("SELECT Id, Name FROM Foo");
+			    return values;
+		    }
+		    catch (SqliteException ex)
+		    {
+			    throw;
+		    }
+		    finally
+		    {
+			    _connection.Close();
+		    }
+	    }
+
+	    public void Save(Foo foo)
+	    {
+		    _connection.Open();
+		    try
+		    {
+			    _connection.Execute($"INSERT INTO Foo(Id, Name) VALUES('{foo.Id}', '{foo.Name}')");
+		    }
+		    catch (SqliteException ex)
+		    {
+			    throw;
+		    }
+		    finally
+		    {
+				_connection.Close();
+			}
+		    
+			
+	    }
+    }
+}
